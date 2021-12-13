a6e4dd60c6404b41c444ef7f7cc55a96606e31a8
ahmed <34ahmed.raza@gmail.com>
2021-12-13 13:51:46 -0800

    checking

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/game')
+// mongoose.connect('mongodb://127.0.0.1:27017/game')
+mongoose.connect("mongodb+srv://admin:admin@first-cluster.pwhl9.mongodb.net/game?retryWrites=true&w=majority")
